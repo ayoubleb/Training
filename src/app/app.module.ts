28b0d3c4dd3b9d1187d35014e2933a7d03a641c0
ayoub leb <lebhal@gmail.com>
2019-02-26 11:58:21 +0100

    day2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule,FormsModules } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 
